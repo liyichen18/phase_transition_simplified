19bc5b4baadbb4d5a5258459f4fb3f76c483fa6e
Jiaqi Zhang <jiaqi2@clemson.edu>
2022-04-13 22:57:34 -0400

    add dimensionless group

--- phase_transition.cc
+++ phase_transition.cc
@@ -723,6 +723,24 @@ void BlockDiagonalPreconditioner<PreconditionerA, PreconditionerS>::vmult(
 
 } // namespace LinearSolvers
 
+
+namespace DimensionlessGroups
+{
+  const double G      = 1.159722e+06; // 1/G characterises Thompson-Gibbs effect
+  const double Pi_T   = 1.452778e+04; // sensible heat / surface tension (AC/CH)
+  const double one_over_Pi_T = 1.0 / Pi_T;
+  const double Pi_eta = 1.101074e+09; // sensible heat / visicosity 
+  const double one_over_Pi_eta = 1.0 / Pi_eta;
+  const double Ste    = 1.252695e-02; // Stefan number
+  const double one_over_Ste = 1.0 / Ste;
+  const double We     = 9.801738e-07; // Weber number
+  const double one_over_We = 1./ We; 
+  const double Re     = 7.428828e-02; // Reynolds number
+  const double one_over_Re = 1.0/Re;
+  const double Pe     = 1.000000e+00; // Peclet number
+  const double one_over_Pe = 1.0/Pe;
+} // namespace DimensionlessGroups
+
 template <int dim>
 class StokesProblem
 {
@@ -766,7 +784,6 @@ private:
     const unsigned int velocity_degree;
     const unsigned int quadrature_degree;
 
-    const double       viscosity;
     MPI_Comm     mpi_communicator;
 
     FESystem<dim>                             fe;
@@ -1904,7 +1921,7 @@ void StokesProblem<dim>::assemble_system(const bool assemble_matrix)
 
               const double log_t_ts_tm = std::log(temperature_ts/melting_t);
               // TM(1-T_ts/TM) + T_ts*log(T_ts/TM), used in  w3 and w5
-              const double w35_reuse_term1 = melting_t * (1. - temperature_ts/melting_t) + temperature_ts * log_t_ts_tm;
+              const double w35_reuse_term1 = (melting_t * (1. - temperature_ts/melting_t) + temperature_ts * log_t_ts_tm) * DimensionlessGroups::Pi_T;
               // w(phi_ch_ts) + 0.5 |grad_psi_ac_ts|^2
               const double w3_reuse_term2 = w_psi_ac_ts + 0.5 * (grad_psi_ac_ts * grad_psi_ac_ts);
 
@@ -1992,7 +2009,6 @@ void StokesProblem<dim>::assemble_system(const bool assemble_matrix)
                     {
                       for (unsigned int j = 0; j < dofs_per_cell; ++j)
                         {
-
                           // w1,2
                           mat = shape_h_ts[j] * shape_phi_ch[i] + mobility_phi * grad_shape_mu_phi_ch[j] * grad_shape_phi_ch[i];
 
@@ -2001,16 +2017,22 @@ void StokesProblem<dim>::assemble_system(const bool assemble_matrix)
                           mat += shape_mu_phi_ch[j] * shape_mu_phi_ch[i];
                           //  term ii
                           mat += (-((r_prime_psi_ac_ts * r_prime_phi_ch_ts * shape_psi_ac_theta[j]
-                                     + r_psi_ac_ts * r_prime_prime_phi_ch_ts * shape_phi_ch_theta[j])
-                                    * latent_heat * (1. - temperature_ts/melting_t))
-                                  + r_psi_ac_ts * r_prime_phi_ch_ts * latent_heat/melting_t * shape_temperature_theta[j]
+                                     + r_psi_ac_ts * r_prime_prime_phi_ch_ts * shape_phi_ch_theta[j]
+                                     ) * latent_heat * (1. - temperature_ts/melting_t)
+                                    ) * DimensionlessGroups::G
+                                  + r_psi_ac_ts * r_prime_phi_ch_ts * latent_heat/melting_t * shape_temperature_theta[j] * DimensionlessGroups::G
                                   + (shape_c_partial_phi_theta[j] * w35_reuse_term1
-                                     + c_partial_phi_ch_ts * shape_temperature_theta[j] * log_t_ts_tm)) * shape_mu_phi_ch[i];
+                                     + c_partial_phi_ch_ts * shape_temperature_theta[j] * log_t_ts_tm * DimensionlessGroups::Pi_T
+                                     )
+                                  ) * shape_mu_phi_ch[i];
                           //  term iii
                           mat += (-(lambda_phi * w_prime_prime_phi_ch_ts
-                                    + lambda_psi * r_prime_prime_phi_ch_ts * w3_reuse_term2) * shape_phi_ch_theta[j]
+                                    + lambda_psi * r_prime_prime_phi_ch_ts * w3_reuse_term2
+                                    ) * shape_phi_ch_theta[j]
                                   - lambda_psi * r_prime_phi_ch_ts * (w_prime_psi_ac_ts * shape_psi_ac_theta[j] + grad_psi_ac_ts * grad_shape_psi_ac_theta[j])
-                                  - (shape_pressure[j] * inv_rho_partial_phi_ch_ts + pressure_star[q] * shape_inv_rho_partial_phi_theta[j])) * shape_mu_phi_ch[i];
+                                  - (shape_pressure[j] * inv_rho_partial_phi_ch_ts + pressure_star[q] * shape_inv_rho_partial_phi_theta[j]
+                                     ) * DimensionlessGroups::We
+                                  ) * shape_mu_phi_ch[i];
                           //  term iv
                           mat += - lambda_phi * (grad_shape_phi_ch_theta[j] * grad_shape_mu_phi_ch[i])
                               -  lambda_phi * shape_rho_theta[j] * (grad_phi_ch_ts * grad_inv_rho_ts) * shape_mu_phi_ch[i]
@@ -2030,14 +2052,15 @@ void StokesProblem<dim>::assemble_system(const bool assemble_matrix)
                           //  term ii
                           mat += ( -( (r_prime_prime_psi_ac_ts * r_phi_ch_ts * shape_psi_ac_theta[j]
                                        + r_prime_psi_ac_ts * r_prime_phi_ch_ts * shape_phi_ch_theta[j])
-                                      * latent_heat * (1. - temperature_ts/melting_t))
-                                   + r_prime_psi_ac_ts * r_phi_ch_ts * latent_heat/melting_t * shape_temperature_theta[j]
-                                   + (shape_c_partial_psi_theta[j] * w35_reuse_term1
-                                      + c_partial_psi_ac_ts * shape_temperature_theta[j] * log_t_ts_tm)) * shape_mu_psi_ac[i];
+                                      * latent_heat * (1. - temperature_ts/melting_t)) * DimensionlessGroups::G
+                                   + r_prime_psi_ac_ts * r_phi_ch_ts * latent_heat/melting_t * shape_temperature_theta[j] * DimensionlessGroups::G
+                                   + (shape_c_partial_psi_theta[j] * w35_reuse_term1 /*multiplied by Pi_T already*/
+                                      + c_partial_psi_ac_ts * shape_temperature_theta[j] * log_t_ts_tm * DimensionlessGroups::Pi_T)) * shape_mu_psi_ac[i];
                           //  term iii
                           mat += (-(r_prime_phi_ch_ts * shape_phi_ch_theta[j] * lambda_psi * w_prime_psi_ac_ts
                                     + r_phi_ch_ts * lambda_psi * w_prime_prime_psi_ac_ts * shape_psi_ac_theta[j])
-                                  -  (shape_pressure[j] * inv_rho_partial_psi_ac_ts + pressure_star[q] * shape_inv_rho_partial_psi_theta[j])) * shape_mu_psi_ac[i];
+                                  -  (shape_pressure[j] * inv_rho_partial_psi_ac_ts + pressure_star[q] * shape_inv_rho_partial_psi_theta[j]) * DimensionlessGroups::We 
+                                  ) * shape_mu_psi_ac[i];
                           //  term iv
                           mat += - (r_prime_phi_ch_ts * shape_phi_ch_theta[j] * lambda_psi * grad_psi_ac_ts
                                     + r_phi_ch_ts * lambda_psi * grad_shape_psi_ac_theta[j]) * grad_shape_mu_psi_ac[i]
@@ -2051,8 +2074,8 @@ void StokesProblem<dim>::assemble_system(const bool assemble_matrix)
                                   + rho_ts * (shape_vel[j]/present_timestep + grad_shape_vel[j] * vel_bar * theta
                                               + 0.5 * div_vel_bar * shape_vel_theta[j])) * shape_vel[i]
                               -  shape_pressure[j] * shape_div_vel[i]
-                              +  scalar_product(shape_eta_theta[j] * e_ts + eta_ts * shape_e_theta[j], grad_shape_vel[i])
-                              -  scalar_product(shape_rho_theta[j] * gamma_ts + rho_ts * shape_gamma_theta[j], grad_shape_vel[i]);
+                              +  scalar_product(shape_eta_theta[j] * e_ts + eta_ts * shape_e_theta[j], grad_shape_vel[i]) * DimensionlessGroups::one_over_Re
+                              -  scalar_product(shape_rho_theta[j] * gamma_ts + rho_ts * shape_gamma_theta[j], grad_shape_vel[i]) * DimensionlessGroups::one_over_We;
 
                           // w7
                           mat += (- theta * shape_div_vel[j]
@@ -2067,12 +2090,12 @@ void StokesProblem<dim>::assemble_system(const bool assemble_matrix)
                                                       + vel_ts * grad_shape_temperature_theta[j])) * shape_temperature[i];
                           //  term ii
                           mat +=(-2. * ( mobility_phi * (grad_mu_phi_ch_star[q] * grad_shape_mu_phi_ch[j])
-                                         + mobility_psi * mu_psi_ac_star[q] * shape_mu_psi_ac[j])
-                                 -  shape_eta_theta[j] * scalar_product(e_ts, grad_vel_ts)
-                                 -  eta_ts * scalar_product(shape_e_theta[j], grad_vel_ts)
-                                 - eta_ts * scalar_product(e_ts, grad_shape_vel[j]) * theta) * shape_temperature[i];
+                                         + mobility_psi * mu_psi_ac_star[q] * shape_mu_psi_ac[j]) * DimensionlessGroups::one_over_Pi_T
+                                 -  shape_eta_theta[j] * scalar_product(e_ts, grad_vel_ts) * DimensionlessGroups::one_over_Pi_eta
+                                 -  eta_ts * scalar_product(shape_e_theta[j], grad_vel_ts) * DimensionlessGroups::one_over_Pi_eta
+                                 -  eta_ts * scalar_product(e_ts, grad_shape_vel[j]) * theta * DimensionlessGroups::one_over_Pi_eta) * shape_temperature[i];
                           //  term iii
-                          mat += thermal_conductivity * grad_shape_temperature_theta[j] * grad_shape_temperature[i];
+                          mat += thermal_conductivity * grad_shape_temperature_theta[j] * grad_shape_temperature[i] * DimensionlessGroups::one_over_Pe;
                           //  term iv
                           mat += latent_heat/melting_t * (r_prime_psi_ac_ts * shape_psi_ac_theta[j] * r_prime_phi_ch_ts * h_ts
                                                           + r_psi_ac_ts * r_prime_prime_phi_ch_ts * shape_phi_ch_theta[j] * h_ts
@@ -2080,10 +2103,10 @@ void StokesProblem<dim>::assemble_system(const bool assemble_matrix)
                                                           - r_prime_prime_psi_ac_ts * shape_psi_ac_theta[j] * r_phi_ch_ts * mobility_psi * mu_psi_ac_star[q]
                                                           - r_prime_psi_ac_ts * r_prime_phi_ch_ts * shape_phi_ch_theta[j] * mobility_psi * mu_psi_ac_star[q]
                                                           - r_prime_psi_ac_ts * r_phi_ch_ts * mobility_psi * shape_mu_psi_ac[j])
-                              * temperature_ts * shape_temperature[i]
+                              * temperature_ts * shape_temperature[i] * DimensionlessGroups::one_over_Ste
                               + latent_heat/melting_t * (r_psi_ac_ts * r_prime_phi_ch_ts * h_ts
                                                          - r_prime_psi_ac_ts * r_phi_ch_ts * mobility_psi * mu_psi_ac_star[q])
-                              * shape_temperature_theta[j] * shape_temperature[i];
+                              * shape_temperature_theta[j] * shape_temperature[i] * DimensionlessGroups::one_over_Ste;
                           //  term v
                           mat += (shape_c_partial_phi_theta[j] * h_ts + c_partial_phi_ch_ts * shape_h_ts[j]
                                   - shape_c_partial_psi_theta[j] * mobility_psi * mu_psi_ac_star[q]
@@ -2115,11 +2138,11 @@ void StokesProblem<dim>::assemble_system(const bool assemble_matrix)
                   //  term i
                   rhs += - mu_phi_ch_star[q] * shape_mu_phi_ch[i];
                   //  term ii
-                  rhs += (r_psi_ac_ts * r_prime_phi_ch_ts * latent_heat * (1. - temperature_ts/melting_t)
+                  rhs += (r_psi_ac_ts * r_prime_phi_ch_ts * latent_heat * (1. - temperature_ts/melting_t) * DimensionlessGroups::G
                           - c_partial_phi_ch_ts * w35_reuse_term1) * shape_mu_phi_ch[i];
                   //  term iii
                   rhs += (lambda_phi * w_prime_phi_ch_ts + lambda_psi * r_prime_phi_ch_ts * w3_reuse_term2
-                          + pressure_star[q] * inv_rho_partial_phi_ch_ts) * shape_mu_phi_ch[i];
+                          + pressure_star[q] * inv_rho_partial_phi_ch_ts * DimensionlessGroups::We) * shape_mu_phi_ch[i];
                   //  term iv
                   rhs += lambda_phi * (grad_phi_ch_ts * grad_shape_mu_phi_ch[i])
                       +  lambda_phi * rho_ts * (grad_phi_ch_ts * grad_inv_rho_ts) * shape_mu_phi_ch[i];
@@ -2131,10 +2154,11 @@ void StokesProblem<dim>::assemble_system(const bool assemble_matrix)
                   //  term i
                   rhs += - mu_psi_ac_star[q] * shape_mu_psi_ac[i];
                   //  term ii
-                  rhs += (r_prime_psi_ac_ts * r_phi_ch_ts * latent_heat * (1. - temperature_ts/melting_t)
-                          - c_partial_psi_ac_ts * w35_reuse_term1) * shape_mu_psi_ac[i];
+                  rhs += (r_prime_psi_ac_ts * r_phi_ch_ts * latent_heat * (1. - temperature_ts/melting_t) * DimensionlessGroups::G
+                          - c_partial_psi_ac_ts * w35_reuse_term1 /*multiplied by Pi_T already*/ ) * shape_mu_psi_ac[i];
                   //  term iii
-                  rhs += (r_phi_ch_ts * lambda_psi * w_prime_psi_ac_ts + pressure_star[q] * inv_rho_partial_psi_ac_ts) * shape_mu_psi_ac[i];
+                  rhs += (r_phi_ch_ts * lambda_psi * w_prime_psi_ac_ts + DimensionlessGroups::We * pressure_star[q] * inv_rho_partial_psi_ac_ts
+                         ) * shape_mu_psi_ac[i];
                   //  term iv
                   rhs += lambda_psi * r_phi_ch_ts * (grad_psi_ac_ts * grad_shape_mu_psi_ac[i])
                       +  lambda_psi * rho_ts * r_phi_ch_ts * (grad_psi_ac_ts * grad_inv_rho_ts) * shape_mu_psi_ac[i];
@@ -2142,8 +2166,8 @@ void StokesProblem<dim>::assemble_system(const bool assemble_matrix)
                   // w6
                   rhs += - rho_ts * D_vel_Dt_ts * shape_vel[i]
                       +    pressure_star[q] * shape_div_vel[i]
-                      -    eta_ts * scalar_product(e_ts, grad_shape_vel[i])
-                      +    rho_ts * scalar_product(gamma_ts, grad_shape_vel[i]);
+                      -    eta_ts * scalar_product(e_ts, grad_shape_vel[i]) * DimensionlessGroups::one_over_Re
+                      +    rho_ts * scalar_product(gamma_ts, grad_shape_vel[i]) * DimensionlessGroups::one_over_We;
 
                   // w7
                   rhs += (div_vel_ts - inv_rho_partial_phi_ch_ts * h_ts
@@ -2153,15 +2177,15 @@ void StokesProblem<dim>::assemble_system(const bool assemble_matrix)
                   //  term i
                   rhs += - rho_ts * c_ts * D_temperature_Dt_ts * shape_temperature[i];
                   //  term ii
-                  rhs += (mobility_phi * (grad_mu_phi_ch_star[q] * grad_mu_phi_ch_star[q])
-                          + mobility_psi * (mu_psi_ac_star[q] * mu_psi_ac_star[q])
-                          + eta_ts * scalar_product(e_ts, grad_vel_ts)) * shape_temperature[i];
+                  rhs += (mobility_phi * (grad_mu_phi_ch_star[q] * grad_mu_phi_ch_star[q]) * DimensionlessGroups::one_over_Pi_T
+                          + mobility_psi * (mu_psi_ac_star[q] * mu_psi_ac_star[q]) * DimensionlessGroups::one_over_Pi_T
+                          + eta_ts * scalar_product(e_ts, grad_vel_ts) * DimensionlessGroups::one_over_Pi_eta) * shape_temperature[i];
                   //  term iii
-                  rhs += - thermal_conductivity * grad_temperature_ts * grad_shape_temperature[i];
+                  rhs += - thermal_conductivity * grad_temperature_ts * grad_shape_temperature[i] * DimensionlessGroups::one_over_Pe;
                   //  term iv
                   rhs += - (latent_heat * r_psi_ac_ts * r_prime_phi_ch_ts * h_ts
                             - r_prime_psi_ac_ts * r_phi_ch_ts * mobility_psi * mu_psi_ac_star[q])
-                      * temperature_ts/melting_t * shape_temperature[i];
+                      * temperature_ts/melting_t * shape_temperature[i] * DimensionlessGroups::one_over_Ste;
                   // term v
                   rhs += - (c_partial_phi_ch_ts * h_ts - c_partial_psi_ac_ts * mobility_psi * mu_psi_ac_star[q])
                       * temperature_ts * log_t_ts_tm * shape_temperature[i];
