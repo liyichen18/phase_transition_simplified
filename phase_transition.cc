104a897f3859124d1d1aa4f1cdcbbc685339dc0f
liyichen <liyichen@vt.edu>
2022-08-16 12:30:41 -0400

    tmp152

--- phase_transition.cc
+++ phase_transition.cc
@@ -1147,7 +1147,7 @@ StokesProblem<dim>::StokesProblem(unsigned int    velocity_degree,
   , eta_s(100.)
   , eta_g(9.670022e-03)
   , surface_tension_phi_ch(1.)
-  , surface_tension_psi_ac(1.)
+  , surface_tension_psi_ac(0.1)
   , lambda_phi(InlineFunctions::compute_lambda_from_h(density_l, surface_tension_phi_ch, eps, 0.0115298)) // surface tension formula is changed, need to compute the factor. 
   , lambda_psi(InlineFunctions::compute_lambda_from_h(density_l, surface_tension_phi_ch, eps, 0.159505)) /*which density should be used? for water ice  h= 0.159505, g=0.1594389483*/ 
   , mobility_phi(1e-4)
@@ -2208,7 +2208,7 @@ void StokesProblem<dim>::assemble_system(const bool assemble_matrix)
               const double r_prime_prime_phi_ch_ts = InlineFunctions::r_prime_prime(phi_ch_ts);
               const double r_prime_prime_psi_ac_ts = InlineFunctions::r_prime_prime(psi_ac_ts);
               const double artificial_diffusion_coefficient
-              = InlineFunctions::r(0.2) - InlineFunctions::r(std::min(phi_ch_n[q], 0.2));
+              = InlineFunctions::r(0.1) - InlineFunctions::r(std::min(phi_ch_n[q], 0.1)); //0.1
 #else
               const double r_alpha = 0.05;
               const double r_phi_ch_ts = InlineFunctions::new_r(phi_ch_ts, r_alpha);
@@ -3328,7 +3328,7 @@ template <int dim>
 void
 StokesProblem<dim>::run()
 {
-  const std::string prefix = "tmp151/";
+  const std::string prefix = "tmp152/";
 
   output_dir      = "./output/" + prefix;
   checkpoints_dir = "./checkpoints/" + prefix;
@@ -3368,7 +3368,7 @@ StokesProblem<dim>::run()
 
         setup_initial_condition();
         relax_phase_field = true;
-        const unsigned int n_relaxation_steps = 0;
+        const unsigned int n_relaxation_steps = 3;
         if(relax_phase_field)
           for(unsigned int i=0; i<n_relaxation_steps; ++i)
             {
