b8813c659010b03f25473ac5de487dec130ac460
liyichen <liyichen@vt.edu>
2022-08-17 01:31:17 -0400

    tmp165

--- phase_transition.cc
+++ phase_transition.cc
@@ -2208,7 +2208,7 @@ void StokesProblem<dim>::assemble_system(const bool assemble_matrix)
               const double r_prime_prime_phi_ch_ts = InlineFunctions::r_prime_prime(phi_ch_ts);
               const double r_prime_prime_psi_ac_ts = InlineFunctions::r_prime_prime(psi_ac_ts);
               const double artificial_diffusion_coefficient
-              = InlineFunctions::r(0.01) - InlineFunctions::r(std::min(phi_ch_n[q], 0.01)); //0.1
+              = InlineFunctions::r(0.0) - InlineFunctions::r(std::min(phi_ch_n[q], 0.0)); //0.1
 #else
               const double r_alpha = 0.05;
               const double r_phi_ch_ts = InlineFunctions::new_r(phi_ch_ts, r_alpha);
@@ -3332,7 +3332,7 @@ template <int dim>
 void
 StokesProblem<dim>::run()
 {
-  const std::string prefix = "tmp164/";
+  const std::string prefix = "tmp165/";
 
   output_dir      = "./output/" + prefix;
   checkpoints_dir = "./checkpoints/" + prefix;
