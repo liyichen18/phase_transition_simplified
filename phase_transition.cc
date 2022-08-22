31ec639895e3a2aacc14752b702d68f23b873b5e
liyichen <liyichen@vt.edu>
2022-08-22 13:36:30 -0400

    tmp203

--- phase_transition.cc
+++ phase_transition.cc
@@ -1150,7 +1150,7 @@ StokesProblem<dim>::StokesProblem(unsigned int    velocity_degree,
   , surface_tension_psi_ac(0.1)
   , lambda_phi(InlineFunctions::compute_lambda_from_h(density_l, surface_tension_phi_ch, eps, 0.0115298)) // surface tension formula is changed, need to compute the factor. 
   , lambda_psi(InlineFunctions::compute_lambda_from_h(density_l, surface_tension_phi_ch, eps, 0.159505)) /*which density should be used? for water ice  h= 0.159505, g=0.1594389483*/ 
-  , mobility_phi(1e-3)
+  , mobility_phi(1e-2)
   , mobility_psi(1e-0)
   , latent_heat(0.0)
   , melting_t(1.)
@@ -3329,7 +3329,7 @@ template <int dim>
 void
 StokesProblem<dim>::run()
 {
-  const std::string prefix = "tmp202/";
+  const std::string prefix = "tmp203/";
 
   output_dir      = "./output/" + prefix;
   checkpoints_dir = "./checkpoints/" + prefix;
