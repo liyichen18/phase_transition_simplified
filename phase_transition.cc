e1314a42e822b8266d52df8c04b67d73c0f8d09f
liyichen <liyichen@vt.edu>
2022-08-23 13:34:02 -0400

    tmp209

--- phase_transition.cc
+++ phase_transition.cc
@@ -3335,7 +3335,7 @@ template <int dim>
 void
 StokesProblem<dim>::run()
 {
-  const std::string prefix = "tmp208/";
+  const std::string prefix = "tmp209/";
 
   output_dir      = "./output/" + prefix;
   checkpoints_dir = "./checkpoints/" + prefix;
@@ -3375,7 +3375,7 @@ StokesProblem<dim>::run()
 
         setup_initial_condition();
         relax_phase_field = true;
-        const unsigned int n_relaxation_steps = 0;
+        const unsigned int n_relaxation_steps = 3;
         if(relax_phase_field)
           for(unsigned int i=0; i<n_relaxation_steps; ++i)
             {
