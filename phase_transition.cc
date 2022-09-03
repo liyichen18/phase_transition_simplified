756182a5c50db3f418a5a6f877808f00f139b097
liyichen <liyichen@vt.edu>
2022-09-03 11:40:35 -0400

    tmp266

--- phase_transition.cc
+++ phase_transition.cc
@@ -918,13 +918,13 @@ void BlockDiagonalPreconditioner<PreconditionerA, PreconditionerS>::vmult(
 
 namespace DimensionlessGroups
 {
-  const double G      = 1.159722e+06; // 1/G characterises Thompson-Gibbs effect
-  const double Pi_T   = 1.452778e+04; // sensible heat / surface tension (AC/CH)
-  const double Pi_eta = 1.090104e+06; // sensible heat / visicosity
-  const double Ste    = 1.252695e-02; // Stefan number
-  const double We     = 1.000000e+00; // Weber number
-  const double Re     = 7.503584e+01; // Reynolds number
-  const double Pe     = 1.010063e+03; // Peclet number
+  const double G      =1.;//  1.159722e+06; // 1/G characterises Thompson-Gibbs effect
+  const double Pi_T   =1.;//   1.452778e+04; // sensible heat / surface tension (AC/CH)
+  const double Pi_eta =1.;//   1.090104e+06; // sensible heat / visicosity
+  const double Ste    =1.;//   1.252695e-02; // Stefan number
+  const double We     =1.;//   1.000000e+00; // Weber number
+  const double Re     =1.;//   7.503584e+01; // Reynolds number
+  const double Pe     =1.;//   1.010063e+03; // Peclet number
 
   const double one_over_Pe = 1.0/Pe;
   const double one_over_Pi_T = 1.0 / Pi_T;
@@ -1152,7 +1152,7 @@ StokesProblem<dim>::StokesProblem(unsigned int    velocity_degree,
   , lambda_psi(InlineFunctions::compute_lambda_from_h(density_l, surface_tension_phi_ch, eps, 0.159505)) /*which density should be used? for water ice  h= 0.159505, g=0.1594389483*/ 
   , mobility_phi(1e-4)
   , mobility_psi(1e-0)
-  , latent_heat(0.1)
+  , latent_heat(0.0)
   , melting_t(100.)
   , k_l(1.) //  thermal_conductivity(1.)
   , k_s(3.994602e+00)
@@ -3328,7 +3328,7 @@ template <int dim>
 void
 StokesProblem<dim>::run()
 {
-  const std::string prefix = "tmp265/";
+  const std::string prefix = "tmp266/";
 
   output_dir      = "./output/" + prefix;
   checkpoints_dir = "./checkpoints/" + prefix;
