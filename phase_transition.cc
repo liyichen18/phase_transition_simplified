143eca4afb05f42793de78f010398d55292b646b
liyichen <liyichen@vt.edu>
2022-09-07 03:57:56 -0400

    tmp283

--- phase_transition.cc
+++ phase_transition.cc
@@ -1157,8 +1157,8 @@ StokesProblem<dim>::StokesProblem(unsigned int    velocity_degree,
   , k_l(1.) //  thermal_conductivity(1.)
   , k_s(3.994602e+00)
   , k_g(4.383266e-02)
-  , initial_temperature(100.)
-  , boundary_temperature(100.)
+  , initial_temperature(98.)
+  , boundary_temperature(98.)
   , ambient_pressure(0.)
   , mapping(1)
   , static_contact_angle(numbers::PI/2.) // 73.47 degrees
@@ -3328,7 +3328,7 @@ template <int dim>
 void
 StokesProblem<dim>::run()
 {
-  const std::string prefix = "tmp282/";
+  const std::string prefix = "tmp283/";
 
   output_dir      = "./output/" + prefix;
   checkpoints_dir = "./checkpoints/" + prefix;
