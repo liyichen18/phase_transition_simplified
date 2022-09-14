65ee51fb63fd4929de639c0bc624deca689b72b5
liyichen <liyichen@vt.edu>
2022-09-13 22:12:47 -0400

    tmp293

--- phase_transition.cc
+++ phase_transition.cc
@@ -26,7 +26,7 @@
 
 #define FORCE_USE_OF_TRILINOS
 #define USE_DIRECT_SOLVER // direct solver cannot be used with block matrix
-#define USE_AXISYMMETRY // axisymmetric implementation
+//#define USE_AXISYMMETRY // axisymmetric implementation
 // #define USE_NEW_R
 
 namespace LA
@@ -918,13 +918,13 @@ void BlockDiagonalPreconditioner<PreconditionerA, PreconditionerS>::vmult(
 
 namespace DimensionlessGroups
 {
-  const double G      =1.; // 1.159722e+06; // 1/G characterises Thompson-Gibbs effect
-  const double Pi_T   =1.; //  1.452778e+04; // sensible heat / surface tension (AC/CH)
-  const double Pi_eta =1.; //  1.090104e+06; // sensible heat / visicosity
-  const double Ste    =1.; //  1.252695e-02; // Stefan number
-  const double We     =1.; //  1.000000e+00; // Weber number
-  const double Re     =1.; //  7.503584e+01; // Reynolds number
-  const double Pe     =1.; //  1.010063e+03; // Peclet number
+  const double G      =2.000000e+01; // 1.159722e+06; // 1/G characterises Thompson-Gibbs effect
+  const double Pi_T   =8.000e+00; //  1.452778e+04; // sensible heat / surface tension (AC/CH)
+  const double Pi_eta =1.6e+01;//  1.090104e+06; // sensible heat / visicosity
+  const double Ste    =4.000e-0;  //  1.252695e-02; // Stefan number
+  const double We     =1.250000e-01;  //  1.000000e+00; // Weber number
+  const double Re     =2.500000e-01;  //  7.503584e+01; // Reynolds number
+  const double Pe     =1.000000e+00;  //  1.010063e+03; // Peclet number
 
   const double one_over_Pe = 1.0/Pe;
   const double one_over_Pi_T = 1.0 / Pi_T;
@@ -1132,12 +1132,12 @@ StokesProblem<dim>::StokesProblem(unsigned int    velocity_degree,
   , n_refinement(7)
   , density_l(1.)  
   , density_s(9.162000e-01 * density_l)
-  , density_g(1. * density_l)
+  , density_g(0.01 * density_l)
   , product_density_g_c_g(3.106963e-04)
   , inv_density_s(1. / density_s)
   , inv_density_l(1. / density_l)
   , inv_density_g(1. / density_g)
-  , present_timestep(1e-3)
+  , present_timestep(5e-3)
   , old_timestep(present_timestep)
   , fix_timestep(present_timestep)
   , cl(1.)
@@ -1147,18 +1147,18 @@ StokesProblem<dim>::StokesProblem(unsigned int    velocity_degree,
   , eta_s(100.)
   , eta_g(9.670022e-03)
   , surface_tension_phi_ch(1.)
-  , surface_tension_psi_ac(0.1)
-  , lambda_phi(InlineFunctions::compute_lambda_from_h(density_l, surface_tension_phi_ch, eps, 0.166667)) // surface tension formula is changed, need to compute the factor.
+  , surface_tension_psi_ac(1.)
+  , lambda_phi(InlineFunctions::compute_lambda_from_h(density_l, surface_tension_phi_ch, eps, 0.0115298)) // surface tension formula is changed, need to compute the factor.
  , lambda_psi(InlineFunctions::compute_lambda_from_h(density_l, surface_tension_psi_ac, eps, 0.159505)) /*which density should be used? for water ice  h= 0.159505, g=0.1594389483*/ 
   , mobility_phi(1e-4)
-  , mobility_psi(1e-0)
-  , latent_heat(0.1)
-  , melting_t(100.)
+  , mobility_psi(1e-1)
+  , latent_heat(1)
+  , melting_t(273.)
   , k_l(1.) //  thermal_conductivity(1.)
   , k_s(3.994602e+00)
   , k_g(4.383266e-02)
-  , initial_temperature(100.)
-  , boundary_temperature(100.)
+  , initial_temperature(melting_t+20.)
+  , boundary_temperature(melting_t+20.)
   , ambient_pressure(0.)
   , mapping(1)
   , static_contact_angle(numbers::PI/2.) // 73.47 degrees
@@ -3328,7 +3328,7 @@ template <int dim>
 void
 StokesProblem<dim>::run()
 {
-  const std::string prefix = "tmp291/";
+  const std::string prefix = "tmp293/";
 
   output_dir      = "./output/" + prefix;
   checkpoints_dir = "./checkpoints/" + prefix;
