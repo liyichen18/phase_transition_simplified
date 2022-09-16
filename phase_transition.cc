60fdf1ea25cf5244c5ded3cc6f2c964361ae1e47
liyichen <liyichen@vt.edu>
2022-09-16 12:06:32 -0400

    tmp298

--- phase_transition.cc
+++ phase_transition.cc
@@ -1157,7 +1157,7 @@ StokesProblem<dim>::StokesProblem(unsigned int    velocity_degree,
   , k_l(1.) //  thermal_conductivity(1.)
   , k_s(3.994602e+00)
   , k_g(4.383266e-02)
-  , initial_temperature(melting_t+20.)
+  , initial_temperature(melting_t-20.)
   , boundary_temperature(melting_t-2.)
   , ambient_pressure(0.)
   , mapping(1)
@@ -3328,7 +3328,7 @@ template <int dim>
 void
 StokesProblem<dim>::run()
 {
-  const std::string prefix = "tmp297/";
+  const std::string prefix = "tmp298/";
 
   output_dir      = "./output/" + prefix;
   checkpoints_dir = "./checkpoints/" + prefix;
