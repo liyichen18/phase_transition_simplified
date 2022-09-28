f370641ef3a1102abccc87377c9d96fa1b9c4555
liyichen <liyichen@vt.edu>
2022-09-27 20:30:05 -0400

    tmp310

--- phase_transition.cc
+++ phase_transition.cc
@@ -1137,7 +1137,7 @@ StokesProblem<dim>::StokesProblem(unsigned int    velocity_degree,
   , inv_density_s(1. / density_s)
   , inv_density_l(1. / density_l)
   , inv_density_g(1. / density_g)
-  , present_timestep(5e-2)
+  , present_timestep(5e-3)
   , old_timestep(present_timestep)
   , fix_timestep(present_timestep)
   , cl(1.)
@@ -3339,7 +3339,7 @@ template <int dim>
 void
 StokesProblem<dim>::run()
 {
-  const std::string prefix = "tmp309/";
+  const std::string prefix = "tmp310/";
 
   output_dir      = "./output/" + prefix;
   checkpoints_dir = "./checkpoints/" + prefix;
