aabde28531ae8127e201f998afce899889aa82e6
liyichen <liyichen@vt.edu>
2023-04-20 10:30:33 -0400

    tmp563

--- phase_transition.cc
+++ phase_transition.cc
@@ -803,7 +803,7 @@ namespace InitialConditions
             const double d = R - r;
             const double phi = 0.5 * (1. + std::tanh(d/eps1));
 
-            const double initial_solid_layer = 0.08; // has to below melting temperature
+            const double initial_solid_layer = 0.3; // has to below melting temperature
             const double psi = 0.5 * (1. + std::tanh((y - initial_solid_layer)/eps1));
 
             const double temperature_transition_function = transition_function(y, initial_solid_layer + 0.1, initial_solid_layer+0.2);
@@ -3501,7 +3501,7 @@ template <int dim>
 void
 StokesProblem<dim>::run()
 {
-  const std::string prefix = "tmp562/";
+  const std::string prefix = "tmp563/";
 
   output_dir      = "./output/" + prefix;
   checkpoints_dir = "./checkpoints/" + prefix;
