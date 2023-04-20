6085f53ba60f90b82a64f42d9ed968841acb9d39
liyichen <liyichen@vt.edu>
2023-04-20 10:36:28 -0400

    tmp567

--- phase_transition.cc
+++ phase_transition.cc
@@ -1170,8 +1170,8 @@ StokesProblem<dim>::StokesProblem(unsigned int    velocity_degree,
   , test_case(testcase)
   , n_refinement(7)
   , density_l(1.)  
-  , density_s(1. * density_l)
-  //, density_s(9.162000e-01 * density_l)
+  //, density_s(1. * density_l)
+  , density_s(9.162000e-01 * density_l)
   , density_g(0.01 * density_l)
   , product_density_g_c_g(3.106963e-04)
   , inv_density_s(1. / density_s)
@@ -3501,7 +3501,7 @@ template <int dim>
 void
 StokesProblem<dim>::run()
 {
-  const std::string prefix = "tmp566/";
+  const std::string prefix = "tmp567/";
 
   output_dir      = "./output/" + prefix;
   checkpoints_dir = "./checkpoints/" + prefix;
