96b19901fa3259ab8c1366d432f4c10898a7c662
liyichen <liyichen@vt.edu>
2023-12-06 15:56:24 -0500

    tmp675

--- phase_transition.cc
+++ phase_transition.cc
@@ -1171,7 +1171,7 @@ StokesProblem<dim>::StokesProblem(unsigned int    velocity_degree,
   , test_case(testcase)
   , n_refinement(7)
   , density_l(1.)  
-  , density_s(0.1 * density_l)
+  , density_s(0.01 * density_l)
   , density_g(0.01 * density_l)
   , product_density_g_c_g(3.106963e-04)
   , inv_density_s(1. / density_s)
@@ -1188,8 +1188,8 @@ StokesProblem<dim>::StokesProblem(unsigned int    velocity_degree,
   , eta_g(9.670022e-03)
   , surface_tension_phi_ch(1.)
   , surface_tension_psi_ac(1.)
-  , lambda_phi(InlineFunctions::compute_lambda_from_h(density_l, surface_tension_phi_ch, eps, 0.04748456343467133)) // surface tension formula is changed, need to compute the factor.
-  , lambda_psi(InlineFunctions::compute_lambda_from_h(density_l, surface_tension_psi_ac, eps, 0.03631570517154943)) /*which density should be used? for water ice  h= 0.159505, g=0.1594389483*/ 
+  , lambda_phi(InlineFunctions::compute_lambda_from_h(density_l, surface_tension_phi_ch, eps, 0.011529847368578174)) // surface tension formula is changed, need to compute the factor.
+  , lambda_psi(InlineFunctions::compute_lambda_from_h(density_l, surface_tension_psi_ac, eps, 0.004677921940969939)) /*which density should be used? for water ice  h= 0.159505, g=0.1594389483*/ 
   , mobility_phi(1e-3)
   , mobility_psi(1.)
   , latent_heat(1)
@@ -3497,7 +3497,7 @@ template <int dim>
 void
 StokesProblem<dim>::run()
 {
-  const std::string prefix = "tmp674/";
+  const std::string prefix = "tmp675/";
 
   output_dir      = "./output/" + prefix;
   checkpoints_dir = "./checkpoints/" + prefix;
