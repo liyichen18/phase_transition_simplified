44a1fa30bdad437b589cee364cf8eb637fe86468
liyichen <liyichen@vt.edu>
2025-03-26 14:39:49 -0400

    tmp796

--- phase_transition.cc
+++ phase_transition.cc
@@ -1193,7 +1193,7 @@ private:
     {
       double aux_c = 100;
 
-      double const_temperature = 1.1;//T ∈ [0.9, 1.1] ∗ Tm
+      double const_temperature = 1.01;//T ∈ [0.9, 1.1] ∗ Tm
     } simplified_model_parameters;
 
     
@@ -1281,7 +1281,7 @@ StokesProblem<dim>::StokesProblem(unsigned int    velocity_degree,
   , inv_density_s(1. / density_s)
   , inv_density_l(1. / density_l)
   , inv_density_g(1. / density_g)
-  , present_timestep(1.5e-4)
+  , present_timestep(1.5e-3)
   , old_timestep(present_timestep)
   , fix_timestep(present_timestep)
   , cl(1.)
@@ -1315,6 +1315,7 @@ StokesProblem<dim>::StokesProblem(unsigned int    velocity_degree,
   , one_over_wall_relaxation_gamma(100)
   , wall_velocity(Tensor<1, dim>())
   , use_adaptive_refinement(true)
+  //, use_adaptive_refinement(false)
   , min_mesh_size(0.01)
   , max_mesh_size(0.5)
 {
@@ -4125,7 +4126,7 @@ template <int dim>
 void
 StokesProblem<dim>::run()
 {
-  const std::string prefix = "tmp794/";
+  const std::string prefix = "tmp796/";
 
   output_dir      = "./output/" + prefix;
   checkpoints_dir = "./checkpoints/" + prefix;
