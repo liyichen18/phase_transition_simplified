14b402f3c7b055e406cff190c85fff93fba0f639
liyichen <liyichen@vt.edu>
2025-04-02 15:35:46 -0400

    tmp810

--- phase_transition.cc
+++ phase_transition.cc
@@ -882,7 +882,7 @@ namespace InitialConditions
             const double d = R - r;
             const double phi = 0.5 * (1. + std::tanh(d/eps1));
 
-            const double initial_solid_layer = 0.08; // has to below melting temperature
+            const double initial_solid_layer = 0.2; // has to below melting temperature
             const double psi = 0.5 * (1. + std::tanh((y - initial_solid_layer)/eps1));
 
             // const double temperature_transition_function = transition_function(y, initial_solid_layer + 0.1, initial_solid_layer+0.2);
@@ -1300,7 +1300,7 @@ StokesProblem<dim>::StokesProblem(unsigned int    velocity_degree,
   , lambda_phi(InlineFunctions::compute_lambda_from_h(density_l, surface_tension_phi_ch, eps, 0.0115298)) // surface tension formula is changed, need to compute the factor.
   , lambda_psi(InlineFunctions::compute_lambda_from_h(density_l, surface_tension_psi_ac, eps, 0.159505)) /*which density should be used? for water ice  h= 0.159505, g=0.1594389483*/ 
   #endif
-  , mobility_phi(1e-2)
+  , mobility_phi(1e-4)
   , mobility_psi(1.)
   , latent_heat(1)
   , melting_t(1.)
@@ -4126,7 +4126,7 @@ template <int dim>
 void
 StokesProblem<dim>::run()
 {
-  const std::string prefix = "tmp809/";
+  const std::string prefix = "tmp810/";
 
   output_dir      = "./output/" + prefix;
   checkpoints_dir = "./checkpoints/" + prefix;
