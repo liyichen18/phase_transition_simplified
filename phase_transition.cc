9ed5d906387afbb0c32787a8b1b5224be5841b93
liyichen <liyichen@vt.edu>
2025-04-06 18:10:35 -0400

    tmp818

--- phase_transition.cc
+++ phase_transition.cc
@@ -1195,7 +1195,7 @@ private:
     {
       double aux_c = 100;
 
-      double const_temperature = 0.9;//T ∈ [0.9, 1.1] ∗ Tm
+      double const_temperature = 1.1;//T ∈ [0.9, 1.1] ∗ Tm
     } simplified_model_parameters;
 
     
@@ -4128,7 +4128,7 @@ template <int dim>
 void
 StokesProblem<dim>::run()
 {
-  const std::string prefix = "tmp817/";
+  const std::string prefix = "tmp818/";
 
   output_dir      = "./output/" + prefix;
   checkpoints_dir = "./checkpoints/" + prefix;
