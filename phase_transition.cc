7e32b0ef16fc24241f0f7966a4f7b6bcf0adf2bb
liyichen <liyichen@vt.edu>
2025-04-12 20:05:23 -0400

    tmp833

--- phase_transition.cc
+++ phase_transition.cc
@@ -106,7 +106,7 @@
    {
      static constexpr double aux_c = 100;
 
-     const double const_temperature = 0.9;//T ∈ [0.9, 1.1] ∗ Tm
+     const double const_temperature = 1.1;//T ∈ [0.9, 1.1] ∗ Tm
    };
  
  namespace InlineFunctions
@@ -4146,7 +4146,7 @@
  void
  StokesProblem<dim>::run()
  {
-   const std::string prefix = "tmp832/";
+   const std::string prefix = "tmp833/";
  
    output_dir      = "./output/" + prefix;
    checkpoints_dir = "./checkpoints/" + prefix;
