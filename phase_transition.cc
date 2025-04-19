4763f7d863884ce5e1be9c9105abe5732cafccb8
liyichen <liyichen@vt.edu>
2025-04-19 02:11:44 -0400

    tmp862

--- phase_transition.cc
+++ phase_transition.cc
@@ -106,7 +106,7 @@
    {
      static constexpr double aux_c = 100;
 
-     static constexpr double const_temperature = 1.1;//T ∈ [0.9, 1.1] ∗ Tm
+     static constexpr double const_temperature = 0.9;//T ∈ [0.9, 1.1] ∗ Tm
    };
  
  namespace InlineFunctions
@@ -896,8 +896,9 @@
              const double d = R - r;
              const double phi = 0.5 * (1. + std::tanh(d/eps1));
  
-             const double initial_solid_layer = 0.2; // has to below melting temperature
-             const double psi = 0.5 * (1. + std::tanh((y - initial_solid_layer)/eps1));
+             //const double initial_solid_layer = 0.2; // has to below melting temperature
+             //const double psi = 0.5 * (1. + std::tanh((y - initial_solid_layer)/eps1));
+             const double psi = 1.0;
  
              // const double temperature_transition_function = transition_function(y, initial_solid_layer + 0.1, initial_solid_layer+0.2);
  
@@ -4147,7 +4148,7 @@
  void
  StokesProblem<dim>::run()
  {
-   const std::string prefix = "tmp861/";
+   const std::string prefix = "tmp862/";
  
    output_dir      = "./output/" + prefix;
    checkpoints_dir = "./checkpoints/" + prefix;
