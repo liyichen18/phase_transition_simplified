6e4fdae37cb5cefe8325df7ca5224d17fc10c639
liyichen <liyichen@vt.edu>
2025-05-08 23:42:17 -0400

    tmp898

--- phase_transition.cc
+++ phase_transition.cc
@@ -894,9 +894,10 @@
                }
 
              const double d = R - r;
-             const double phi = 0.5 * (1. + std::tanh(d/eps1));
+             //const double phi = 0.5 * (1. + std::tanh(d/eps1));
+             const double phi = 1;
 
-             const double initial_solid_layer = 0.2; // has to below melting temperature
+             const double initial_solid_layer = 1; // has to below melting temperature
              const double psi = 0.5 * (1. + std::tanh((y - initial_solid_layer)/eps1));
 
              // const double temperature_transition_function = transition_function(y, initial_solid_layer + 0.1, initial_solid_layer+0.2);
@@ -4218,7 +4219,7 @@ void StokesProblem<dim>::reset_aux_q(VectorType &solution)
  void
  StokesProblem<dim>::run()
  {
-   const std::string prefix = "tmp897/";
+   const std::string prefix = "tmp898/";
 
    output_dir      = "./output/" + prefix;
    checkpoints_dir = "./checkpoints/" + prefix;
