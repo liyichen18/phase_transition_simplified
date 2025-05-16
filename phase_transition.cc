da8664679f6756bec298136cabf609d24ed00f31
liyichen <liyichen@vt.edu>
2025-05-16 17:03:38 -0400

    tmp915

--- phase_transition.cc
+++ phase_transition.cc
@@ -106,7 +106,7 @@
    {
      static constexpr double aux_c = 100;
 
-     static constexpr double const_temperature = 0.9;//T ∈ [0.9, 1.1] ∗ Tm
+     static constexpr double const_temperature = 1.1;//T ∈ [0.9, 1.1] ∗ Tm
    };
 
  namespace InlineFunctions
@@ -4219,7 +4219,7 @@ void StokesProblem<dim>::reset_aux_q(VectorType &solution)
  void
  StokesProblem<dim>::run()
  {
-   const std::string prefix = "tmp914/";
+   const std::string prefix = "tmp915/";
 
    output_dir      = "./output/" + prefix;
    checkpoints_dir = "./checkpoints/" + prefix;
