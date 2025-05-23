356f30929c86d413d93445e146b16318bea2caf9
liyichen <liyichen@vt.edu>
2025-05-23 19:21:08 -0400

    tmp921

--- phase_transition.cc
+++ phase_transition.cc
@@ -1302,7 +1302,7 @@
    , inv_density_s(1. / density_s)
    , inv_density_l(1. / density_l)
    , inv_density_g(1. / density_g)
-   , present_timestep(0.01)
+   , present_timestep(0.1e-1)
    , old_timestep(present_timestep)
    , fix_timestep(present_timestep)
    , cl(1.)
@@ -3947,57 +3947,6 @@ void StokesProblem<dim>::reset_aux_q(VectorType &solution)
                               DataOut<dim>::type_dof_data,
                               data_component_interpretation);
 
-    // difference q and f
-        {
-      Vector<double> aux_q_difference(dof_handler.n_dofs());
-
-      const auto &fe = dof_handler.get_fe();
-      const auto component_index_aux_q = extractors.aux_q.component;
-      const auto component_index_phi = extractors.phi_ch.component;
-      const auto component_index_psi = extractors.psi_ac.component;
-
-      const Quadrature<dim> quadrature(fe.get_unit_support_points());
-      FEValues<dim> fe_values(mapping, fe, quadrature, update_values);
-      std::vector<types::global_dof_index> local_dof_indices(fe.dofs_per_cell);
-
-      for (const auto &cell : dof_handler.active_cell_iterators())
-        if (cell->is_locally_owned())
-        {
-          fe_values.reinit(cell);
-          cell->get_dof_indices(local_dof_indices);
-
-          std::vector<double> phi_vals(fe.dofs_per_cell);
-          std::vector<double> psi_vals(fe.dofs_per_cell);
-          std::vector<double> aux_q_vals(fe.dofs_per_cell);
-
-          fe_values[extractors.phi_ch].get_function_values(locally_relevant_solution, phi_vals);
-          fe_values[extractors.psi_ac].get_function_values(locally_relevant_solution, psi_vals);
-          fe_values[extractors.aux_q].get_function_values(locally_relevant_solution, aux_q_vals);
-
-          for (unsigned int i = 0; i < fe.dofs_per_cell; ++i)
-          {
-            const unsigned int comp = fe.system_to_component_index(i).first;
-            if (comp != component_index_aux_q)
-              continue;
-
-            const double F = InlineFunctions::F(phi_vals[i],
-                                                psi_vals[i],
-                                                eps,
-                                                lambda_phi,
-                                                lambda_psi,
-                                                latent_heat,
-                                                initial_temperature,
-                                                melting_t);
-            const double sqrt_val = std::sqrt(F + SimpliedModelParameters::aux_c);
-            aux_q_difference[local_dof_indices[i]] = aux_q_vals[i] - sqrt_val;
-          }
-        }
-
-      aux_q_difference.compress(VectorOperation::insert);
-      data_out.add_data_vector(aux_q_difference, "aux_q_difference");
-    }
-
-
      Vector<float> subdomain(triangulation.n_active_cells());
      for (unsigned int i = 0; i < subdomain.size(); ++i)
          subdomain(i) = triangulation.locally_owned_subdomain();
@@ -4270,7 +4219,7 @@ void StokesProblem<dim>::reset_aux_q(VectorType &solution)
  void
  StokesProblem<dim>::run()
  {
-   const std::string prefix = "tmp920/";
+   const std::string prefix = "tmp921/";
 
    output_dir      = "./output/" + prefix;
    checkpoints_dir = "./checkpoints/" + prefix;
@@ -4408,7 +4357,7 @@ void StokesProblem<dim>::reset_aux_q(VectorType &solution)
          current_solution = old_solution; // u^*, newton initial guess
 
          // 每10步 reset aux_q
-        if (step_number % 300 == 0)
+        if (step_number % 100 == 0)
         {
           pcout << "Resetting aux_q at step " << step_number << std::endl;
           reset_aux_q(old_solution);
