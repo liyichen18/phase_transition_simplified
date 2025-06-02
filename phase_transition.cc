3fc39e81ff91faa4be4fd7661a62823fb596ffd9
liyichen <liyichen@vt.edu>
2025-06-02 00:47:10 -0400

    tmp946

--- phase_transition.cc
+++ phase_transition.cc
@@ -1300,7 +1300,7 @@
    , inv_density_s(1. / density_s)
    , inv_density_l(1. / density_l)
    , inv_density_g(1. / density_g)
-   , present_timestep(0.7e-3)
+   , present_timestep(1e-3)
    , old_timestep(present_timestep)
    , fix_timestep(present_timestep)
    , cl(1.)
@@ -4147,7 +4147,7 @@
  void
  StokesProblem<dim>::run()
  {
-   const std::string prefix = "tmp945/";
+   const std::string prefix = "tmp946/";
  
    output_dir      = "./output/" + prefix;
    checkpoints_dir = "./checkpoints/" + prefix;
