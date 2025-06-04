ddecc4b8a3bd4815a4bb5832a5a80b801be421fc
liyichen <liyichen@vt.edu>
2025-06-04 14:33:50 -0400

    tmp954

--- phase_transition.cc
+++ phase_transition.cc
@@ -1300,7 +1300,7 @@
    , inv_density_s(1. / density_s)
    , inv_density_l(1. / density_l)
    , inv_density_g(1. / density_g)
-   , present_timestep(1e-3)
+   , present_timestep(0.3e-3)
    , old_timestep(present_timestep)
    , fix_timestep(present_timestep)
    , cl(1.)
@@ -4147,7 +4147,7 @@
  void
  StokesProblem<dim>::run()
  {
-   const std::string prefix = "tmp951/";
+   const std::string prefix = "tmp954/";
  
    output_dir      = "./output/" + prefix;
    checkpoints_dir = "./checkpoints/" + prefix;
@@ -4264,7 +4264,7 @@
      while (step_number < max_step_number)
        {
          old_timestep     = present_timestep;
-         present_timestep = std::min(fix_timestep, (1e-5) * std::pow(1.05, step_number));
+         //present_timestep = std::min(fix_timestep, (1e-5) * std::pow(1.05, step_number));
  
          step_number ++;
          runtime += present_timestep;
