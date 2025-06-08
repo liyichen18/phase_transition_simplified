f4d0295dde829248f81d3f6147ddb65bcfe27733
liyichen <liyichen@vt.edu>
2025-06-08 01:57:03 -0400

    tmp962

--- phase_transition.cc
+++ phase_transition.cc
@@ -1322,7 +1322,7 @@
    , lambda_psi(InlineFunctions::compute_lambda_from_h(density_l, surface_tension_psi_ac, eps, 0.159505)) /*which density should be used? for water ice  h= 0.159505, g=0.1594389483*/
    #endif
    , mobility_phi(1e-4)
-   , mobility_psi(100.)
+   , mobility_psi(1.)
    , latent_heat(1)
    , melting_t(1.)
    , k_l(1.) //  thermal_conductivity(1.)
@@ -4270,7 +4270,7 @@ void StokesProblem<dim>::reset_aux_q(VectorType &solution)
  void
  StokesProblem<dim>::run()
  {
-   const std::string prefix = "tmp961/";
+   const std::string prefix = "tmp962/";
 
    output_dir      = "./output/" + prefix;
    checkpoints_dir = "./checkpoints/" + prefix;
