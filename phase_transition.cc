8e171244d6b9c9e3ad6c0624d4f1b91554c9bc7e
liyichen <liyichen@vt.edu>
2025-09-10 17:04:46 -0400

    tmp1036

--- phase_transition.cc
+++ phase_transition.cc
@@ -106,7 +106,7 @@
    {
      static constexpr double aux_c = 100;
 
-     static constexpr double const_temperature = 0.9;//T ∈ [0.9, 1.1] ∗ Tm
+     static constexpr double const_temperature = 1.1;//T ∈ [0.9, 1.1] ∗ Tm
    };
  
  namespace InlineFunctions
@@ -894,12 +894,11 @@
                }
  
              const double d = R - r;
-             //const double phi = 0.5 * (1. + std::tanh(d/eps1));
-             const double phi = 1;
-
-             const double initial_solid_layer = 1; // has to below melting temperature
+             const double phi = 0.5 * (1. + std::tanh(d/eps1));
+ 
+             const double initial_solid_layer = 0.2; // has to below melting temperature
              const double psi = 0.5 * (1. + std::tanh((y - initial_solid_layer)/eps1));
-
+ 
              // const double temperature_transition_function = transition_function(y, initial_solid_layer + 0.1, initial_solid_layer+0.2);
  
              // const double initial_t =
@@ -1302,7 +1301,7 @@
    , inv_density_s(1. / density_s)
    , inv_density_l(1. / density_l)
    , inv_density_g(1. / density_g)
-   , present_timestep(0.1e-1)
+   , present_timestep(0.1e-3)
    , old_timestep(present_timestep)
    , fix_timestep(present_timestep)
    , cl(1.)
@@ -4202,7 +4201,7 @@ double StokesProblem<dim>::compute_total_energy() const
  void
  StokesProblem<dim>::run()
  {
-   const std::string prefix = "tmp1035/";
+   const std::string prefix = "tmp1036/";
  
    output_dir      = "./output/" + prefix;
    checkpoints_dir = "./checkpoints/" + prefix;
