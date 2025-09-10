2266a94c7ca731f746f783e2e4ed85cf5dcfd280
liyichen <liyichen@vt.edu>
2025-09-10 17:02:04 -0400

    tmp1035

--- phase_transition.cc
+++ phase_transition.cc
@@ -1302,7 +1302,7 @@
    , inv_density_s(1. / density_s)
    , inv_density_l(1. / density_l)
    , inv_density_g(1. / density_g)
-   , present_timestep(0.1e-2)
+   , present_timestep(0.1e-1)
    , old_timestep(present_timestep)
    , fix_timestep(present_timestep)
    , cl(1.)
@@ -4202,7 +4202,7 @@ double StokesProblem<dim>::compute_total_energy() const
  void
  StokesProblem<dim>::run()
  {
-   const std::string prefix = "tmp1034/";
+   const std::string prefix = "tmp1035/";
  
    output_dir      = "./output/" + prefix;
    checkpoints_dir = "./checkpoints/" + prefix;
